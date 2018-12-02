ff3f175a0b42d8430e430c1452375bfd8771f9c7
Ananda Poudel <apoudel@stcloudstate.edu>
2018-12-02 13:35:28 -0600

    Scaledown prefab

---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour {
+
+    private void Awake()
+    {
+        GameObject[] myGameObjects = GameObject.FindGameObjectsWithTag("ScaleDown");
+        foreach(var v in myGameObjects)
+        {
+            v.transform.localScale = v.transform.localScale / 1000;
+        }
+    }
+
+}
