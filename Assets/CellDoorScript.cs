e41b627a287810613a39094c75a9c9779302a958
bjeannot <bentjeannot@gmail.com>
2019-04-24 23:12:35 -0500

    Partial Puzzle Element Completion

---
+++ Assets/CellDoorScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellDoorScript : MonoBehaviour
+{
+    public GameObject KeyElement;
+
+    private bool readyToBeUnlocked;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        readyToBeUnlocked = false;
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BeginExplosion()
+    {
+        Invoke("Explode", 5);
+    }
+
+    private void Explode()
+    {
+        animator.SetBool("Explosion", true);
+    }
+
+    public bool IsReadyToBeUnlocked()
+    {
+        return readyToBeUnlocked;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(KeyElement))
+        {
+            //Key element has entered trigger
+            if (other.gameObject.GetComponent<BucketScript>().IsReadyToUnlock())
+            {
+                readyToBeUnlocked = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.Equals(KeyElement))
+        {
+            //Key element has left trigger
+            readyToBeUnlocked = false;
+        }
+    }
+}
---
+++ Assets/SulfurSpotScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SulfurSpotScript : MonoBehaviour
+{
+    public GameObject KeyElement;
+
+    public GameObject PrizeElement;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PrizeElement.SetActive(false);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(KeyElement))
+        {
+            PrizeElement.SetActive(true);
+        }
+    }
+}
